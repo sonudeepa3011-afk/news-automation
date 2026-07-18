be29f7d310c9bfcf34c6dcaf7f9f73d8a9f13426
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-18 08:53:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from newsdata import get_latest_news
+from gemini_ai import write_article
 
 def main():
     try:
         news = get_latest_news()
 
+        title = news.get("title", "")
+        description = news.get("description", "")
+
         print("=" * 50)
         print("LATEST NEWS")
         print("=" * 50)
 
-        print("Title :", news.get("title"))
-        print("Source:", news.get("source_name"))
-        print("Date  :", news.get("pubDate"))
-        print("Link  :", news.get("link"))
+        print(title)
+        print()
+
+        print("Generating Article with Gemini AI...")
+        print()
+
+        article = write_article(title, description)
+
+        print(article)
 
     except Exception as e:
         print("ERROR:", e)
