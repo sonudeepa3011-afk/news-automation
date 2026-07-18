76080a932c14d523d21a8b26e6c4845bf0d5f601
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-18 10:49:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from newsdata import get_latest_news
 from gemini_ai import write_article
 from wordpress import create_draft
+from duplicate import is_duplicate
+
 
 def main():
     try:
+        # Get latest news
         news = get_latest_news()
 
         title = news.get("title", "Untitled News")
         description = news.get("description", "")
 
-        print(f"News: {title}")
+        print(f"News Title: {title}")
 
-        print("Generating article...")
+        # Check duplicate
+        if is_duplicate(title):
+            print("Duplicate news found. Skipping...")
+            return
+
+        # Generate article using Gemini
+        print("Generating AI Article...")
         article = write_article(title, description)
 
-        print("Creating WordPress draft...")
+        # Save to WordPress Draft
+        print("Creating WordPress Draft...")
         result = create_draft(title, article)
 
+        print("===================================")
         print("Draft Created Successfully!")
-        print("Post ID:", result.get("id"))
+        print("Post ID :", result.get("id"))
         print("Post URL:", result.get("link"))
+        print("===================================")
 
     except Exception as e:
-        print("ERROR:", e)
+        print("ERROR:", str(e))
+
 
 if __name__ == "__main__":
     main()
