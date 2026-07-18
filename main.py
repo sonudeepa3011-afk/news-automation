7b79528cf168715279cc839c0fdf8e14c0b48678
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-18 13:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
+from scraper.avenue_mail import get_latest_news
 from gemini_ai import generate_news
 from wordpress import create_draft
 from duplicate import is_duplicate
-from scraper.avenue_mail import get_latest_news
+
 
 def main():
     try:
         print("Fetching latest news...")
 
-        news = get_latest_news()
+        news_list = get_latest_news()
 
-        if not news:
+        if not news_list:
             print("No news found.")
             return
 
-        original_title = news.get("title", "")
-        description = news.get("description", "")
+        print(f"Found {len(news_list)} news articles.")
 
-        # Duplicate Check
-        if is_duplicate(original_title):
-            print("Duplicate news found. Skipping...")
-            return
+        for news in news_list:
 
-        print("Generating AI Article...")
+            title = news["title"]
+            description = news.get("description", "")
 
-        result = generate_news(
-            original_title,
-            description
-        )
+            print(f"\nChecking: {title}")
 
-        print("Creating WordPress Draft...")
+            # Duplicate Check
+            if is_duplicate(title):
+                print("Duplicate Found. Skipping...")
+                continue
 
-        create_draft(
-            result["title"],
-            result["slug"],
-            result["article"],
-            result["meta_description"],
-            result["comma_tags"]
-        )
+            print("Generating AI Article...")
 
-        print("======================================")
+            result = generate_news(
+                title,
+                description
+            )
+
+            print("Creating WordPress Draft...")
+
+            create_draft(
+                result["title"],
+                result["slug"],
+                result["article"],
+                result["meta_description"],
+                result["comma_tags"]
+            )
+
+            print("Draft Created Successfully.\n")
+
+        print("====================================")
         print("Automation Completed Successfully ✅")
-        print("======================================")
+        print("====================================")
 
     except Exception as e:
         print("ERROR:", str(e))
