3460638d61ea3e71b38a8cf142d4408c77b97e9c
sonudeepa3011-afk <sonudeepa3011@gmail.com>
2026-07-18 08:04:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("News Automation Started")
+import os
+import requests
+
+NEWS_API = os.getenv("NEWSDATA_API_KEY")
+
+url = f"https://newsdata.io/api/1/news?apikey={NEWS_API}&country=in&language=en"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text[:500])
